e40514a980f7d2f2eb95e028144fe082aaa9ed7f
Lachezar Lechev <elpiel93@gmail.com>
2021-11-30 17:31:54 +0200

    use SyncClient::all_crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
 
-//pub fn fetch_all_categories(categories: &[&str]) { // -> HashMap? {
+use crates_io_api::{Crate, Error, SyncClient};
 
+
+pub fn fetch_all_categories(client: &SyncClient, categories: &[&str]) -> Result<HashMap<String, Vec<Crate>>, Error> {
+    let mut category_crates = HashMap::new();
+
+    for category in categories {
+
+        // TODO: add crate urlencoded in Cargo.toml
+        // let url_encoded_query = urlencoded
+        // rust_analyzer - VS code
+        let all_crates_of_category = client.all_crates(Some(format!("category={}", category)))
+        .expect("Error listing the crates");
+
+        let previous = category_crates.insert(category.to_string(), all_crates_of_category);
+
+        assert!(previous.is_none())
+    }
     
+    Ok(category_crates)
 
-//}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,36 @@
+use crates_categories::fetch_all_categories;
 //use crates_categories::fetch_all_categories;
-use crates_io_api::{SyncClient, Error, ListOptions, Sort};
-use std::collections::HashMap; 
-
+use crates_io_api::{Error, ListOptions, Sort, SyncClient};
+use std::collections::HashMap;
 
 fn list_top_dependencies() -> Result<(), Error> {
-    
     // Instantiate the client.
     let client = SyncClient::new(
-         "IPCA - Rust",
-         std::time::Duration::from_millis(1000), //Resquests to do per second. 
+        "IPCA - Rust",
+        std::time::Duration::from_millis(1000), //Resquests to do per second.
     )?;
-    
+
     // Retrieve summary data.
     let summary = client.summary()?;
     for c in summary.most_downloaded {
         println!("{}:", c.id);
     }
-    Ok(())        
+    Ok(())
 }
 
-fn main()  {
-    
+fn main() {
     //Have a list of categories (configuration) to fetch from crates.io
     let list_of_categories: [&str; 2] = ["algorithms", "asynchronous"];
-    
-    //create a SyncClient
-    
 
+    //create a SyncClient
     let client = SyncClient::new(
         "IPCA - Rust",
-        std::time::Duration::from_millis(1000), //Resquests to do per second. 
-    ).expect("Error making the client");
-    
-    let category_crates = HashMap::new();
+        std::time::Duration::from_millis(1000), //Resquests to do per second.
+    )
+    .expect("Error making the client");
 
-    for category in list_of_categories {
-        let crates = client.crates(ListOptions{
-            sort: Sort::Alphabetical,
-            per_page: 100,
-            page: 1,
-            query: Some(format!("category={}", category)),
-          }).expect("Error listing the crates");
-        
-    }
-    
+    let results = fetch_all_categories(&client, &list_of_categories)
+        .expect("Should fetch all crates of every category and return them");
 
-
-    match list_top_dependencies() {
-        Ok(()) => println!("I worked and printed the crates :D "),
-        Err(err) => eprintln!("error: not again :( {}", err),
-    };
-    
-    //let categories = fetch_all_categories(&list_of_categories);
-    //println!("Call lib.rs and save results to a file or multiple files)!");
+    // TODO: Save results to file?!
 }
-
-
