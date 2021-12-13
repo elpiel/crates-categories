94292a12ea67ce9822f63735321d728beae273c3
Hélder Barbosa <helderbarbosa.esaf@gmail.com>
2021-12-13 01:50:24 +0000

    Mismatched Types

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crates_io_api::{Crate, Error, SyncClient};
+use crates_io_api::{Crate, Error, SyncClient, FullCrate};
 
 
 pub fn fetch_all_categories(client: &SyncClient, categories: &[&str]) -> Result<HashMap<String, Vec<Crate>>, Error> {
@@ -24,3 +24,21 @@ pub fn fetch_all_categories(client: &SyncClient, categories: &[&str]) -> Result<
     Ok(category_crates)
 
 }
+
+pub fn fetch_full_details(client: &SyncClient, categories_crate: HashMap<&str,&Vec<Crate>> ) -> Result <HashMap<String, FullCrate>, Error> {
+    let mut category_full_data: HashMap<String, FullCrate> = HashMap::new();
+    
+    for (category, krate) in categories_crate{
+        for k in krate{
+            let full_crate = client.full_crate(&k.name, false)
+                .expect("Error fetching a full crate");
+            category_full_data.insert(category.to_string(), full_crate);
+            }
+    }
+   
+    Ok(category_full_data)
+
+}
+
+
+   
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crates_categories::fetch_all_categories;
+use crates_categories::fetch_full_details;
 //use crates_categories::fetch_all_categories;
 use crates_io_api::{SyncClient};
 //use std::collections::HashMap;
@@ -18,7 +19,7 @@ fn main() {
     let categories_crates = fetch_all_categories(&client, &list_of_categories)
         .expect("Should fetch all crates of every category and return them");
 
-    // Just a test to print the crates that we have fetched to the categories_Crates hashmap 
+    // Just a test to print the crates that we have fetched to the categories_Crates hashmap (AND ITS WORKING FINE) you can ignore this
     for (categorie, krate) in categories_crates {
         for x in krate {
             println!("Category: {} Crate {}", categorie ,x.name)
@@ -28,8 +29,18 @@ fn main() {
     
     // TODO: fetch each and every crates for each and every category to get the full details.
     // the `Crate` in `categories_crates` does not contain all the information you need.
-
     // let category_crates_with_full_details = my_function(&client, &categories_crates).expect("Should collect all the information of every crate");
+    let category_crates_with_full_details = fetch_full_details(&client, &categories_crates)
+        .expect("Should collect all the information of every crate");
 
+    
+
+    
+
+    
+    
+    
+    
+    
     // TODO: Save results to file?!
 }
