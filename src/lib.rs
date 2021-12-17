be3c53036728270a22da2b9ed18ba20dc66cc190
Hélder Barbosa <helderbarbosa.esaf@gmail.com>
2021-12-17 09:37:11 +0000

    final presentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{collections::HashMap};
-use crates_io_api::{Crate, Error, SyncClient, FullCrate};
+use crates_io_api::{Crate, Error, SyncClient, FullCrate}; //Structs from crates_io_api
 
 
 pub fn fetch_all_categories(client: &SyncClient, categories: &[&str]) -> Result<HashMap<String, Vec<Crate>>, Error> {
@@ -9,7 +9,7 @@ pub fn fetch_all_categories(client: &SyncClient, categories: &[&str]) -> Result<
 
         
        
-        let all_crates_of_category = client.all_crates(Some(format!("category={}", category)))
+        let all_crates_of_category = client.all_crates(Some(format!("category={}", category)))  //fucntion all_crates from crates.io api
         .expect("Error listing the crates");
 
         let previous = category_crates.insert(category.to_string(), all_crates_of_category);
@@ -25,19 +25,17 @@ pub fn fetch_all_categories(client: &SyncClient, categories: &[&str]) -> Result<
 
 
 pub fn fetch_full_details(client: &SyncClient, categories_crate: &HashMap<String,Vec<Crate>> ) -> Result <HashMap<String, Vec<FullCrate>>, Error> {
-    let mut category_full_data: HashMap<String, Vec<FullCrate>> = HashMap::new();
+    let mut category_full_data: HashMap<String, Vec<FullCrate>> = HashMap::new(); //new hash map to hold as value a vec of FullCrate
     let mut vec = Vec::new() ;
-    for (category, krate) in categories_crate.iter(){
+    for (category, krate) in categories_crate.iter(){ //method .iter() allows us to run trough a collection without taking the ownership
         for k in krate
         {
             let full_crate =  client.full_crate(&k.name, false)
             .expect("error collectin the full crate");
-            vec.push(full_crate);
-            println!("In category : {} Pushed a crate, Vec lenght {}",&category, vec.len()); //Just log for testing will delete later
-
+            vec.push(full_crate); //Pushing full crates into the vector
         }
-        category_full_data.insert(category.to_string(), vec.clone());
-        vec.clear(); 
+        category_full_data.insert(category.to_string(), vec.clone()); //Insert into the hash map a clone of the vec. 
+        vec.clear(); //Clear all the previous crates in this vec. 
     }
    
     Ok(category_full_data)
