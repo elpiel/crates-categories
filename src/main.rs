7c8c82852f158d8ac7835a7a7d85c75957f17db7
Hélder Barbosa <helderbarbosa.esaf@gmail.com>
2021-12-17 01:21:17 +0000

    Final (To presentation)

--- src/main.rs
+++ src/main.rs
@@ -13,39 +13,26 @@ fn main() {
     //create a SyncClient
     let client = SyncClient::new(
         "IPCA - Rust",
-        std::time::Duration::from_millis(1), //Resquests to do per second.
+        std::time::Duration::from_millis(1000), //Resquests to do per second.
     )
     .expect("Error making the client");
 
+    //Fetch all crates from the given list_of_categories to an HashMap - as colection to a vector of crates.
     let categories_crates = fetch_all_categories(&client, &list_of_categories)
         .expect("Should fetch all crates of every category and return them");
 
-    // Just a test to print the crates that we have fetched to the categories_Crates hashmap (AND ITS WORKING FINE) you can ignore this
-    for (categorie, krate) in categories_crates.clone() {
-        for x in krate {
-            println!("Category: {} Crate {}", categorie ,x.name)
-        }
-
-    }
     
     //Fetch each and every crates for each and every category to get the full details.   
     let category_crates_with_full_details = fetch_full_details(&client, &categories_crates)
         .expect("Should collect all the information of every crate");
 
 
-    // Test to print the new Hash map to check if the full crate are correct 
-    for (categorie ,krate ) in category_crates_with_full_details.iter() {
-        for k in krate.iter() {
-            println!("Category: {} Full Crate Name: {}",categorie , k.name);
-        }
-    }
-
     // Serialize the full crates, to .json files for each categorie. 
-    for (categorie, krate   ) in category_crates_with_full_details.iter() {
-        let vec_full_crates_to_json = serde_json::to_string_pretty(krate)
+    for (categorie, krate   ) in category_crates_with_full_details.iter() { //iterate the collection 
+        let vec_full_crates_to_json = serde_json::to_string_pretty(krate) //new vector that holds json string in pretty format.
         .expect("Error serializing");
         
-        fs::write(format!("{}.json", categorie.to_string()), vec_full_crates_to_json)
+        fs::write(format!("{}.json", categorie.to_string()), vec_full_crates_to_json)   //Macro to make the path of the file, and write the json vec on it
         .expect("Failed to write the file");
 
     }
